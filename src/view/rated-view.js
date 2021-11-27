c75ff9cddd9c0a76102e33a716b3551c50731e36
DragaTM <dragatm@gmail.com>
2021-11-27 14:26:27 +0200

    разбито на компоненты

---
+++ src/view/rated-view.js
@@ -0,0 +1,9 @@
+export const createRatedTemplate = () => (
+	`<section class="films-list films-list--rated films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container">
+        
+      </div>
+    </section>`
+	)
