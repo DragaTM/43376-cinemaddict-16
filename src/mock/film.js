7e35ac5231d655e58a67d45a0980f265580bd189
DragaTM <dragatm@gmail.com>
2021-11-29 17:16:00 +0200

    Create film.js

---
+++ src/mock/film.js
@@ -0,0 +1,22 @@
+export const generateFilm = () => ({
+	name: "The Dance of Life";
+	inWatchlist: false;
+	isWatched: false;
+	isFavorite: true;
+	rating: "8.3";
+	year: "1929";
+	time: "1h 55m";
+	genre: "Musical";
+	description: ;
+	comments: "4";
+});
+
+const generateName = () => {
+	const names = [
+		'Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a trs',
+		'Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brants narrow escap',
+		'Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on',
+		'The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti',
+		'In this short, Sindbad the Sailor (presumably Bluto playing a "role") proclaims himself, in song, to be the greatest sailor, adventurer and',
+	]
+}
