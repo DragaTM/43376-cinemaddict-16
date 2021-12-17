d3102c7be4e33ef0348c31a34ac1bc51683dfbfd
dragatm <dragatm@gmail.com>
2021-12-17 18:43:19 +0200

    11

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -7,7 +7,7 @@ import FilmPresenter from './film-presenter.js';
 import CommentedView from '../view/commented-view.js';
 import {render, renderPosition, remove} from '../render.js';
 import {FILM_COUNT, FILM_COUNT_PER_STEP, SortType} from '../const.js';
-import {updateItem, sortDate, sortRating} from '../utils.js';
+import {updateItem} from '../utils.js';
 
 export default class MainPresenter {
   #emptyComponent = new EmptyView();
@@ -116,10 +116,10 @@ export default class MainPresenter {
   #sortFilms = (sortType) => {
     switch (sortType) {
       case SortType.DATE:
-        this.#films.sort(sortDate);
+        this.#films.sort(function(filmA, filmB){return filmA.year - filmB.year;});
         break;
       case SortType.RATING:
-        this.#films.sort(sortRating);
+        this.#films.sort(function(filmA, filmB){return filmA.rating - filmB.rating;});
         break;
       default:
         this.#films = [...this.#soursedFilms];
