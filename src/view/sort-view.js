b031c9e4105ca349827ef32eae702337822649df
dragatm <dragatm@gmail.com>
2021-12-17 19:49:47 +0200

    m5t2

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -3,9 +3,9 @@ import {SortType} from '../const.js';
 
 const createSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active data-sort-view="${SortType.DEFAULT}">Sort by default</a></li>
-    <li><a href="#" class="sort__button" data-sort-view="${SortType.DATE}">Sort by date</a></li>
-    <li><a href="#" class="sort__button" data-sort-view="${SortType.RATING}">Sort by rating</a></li>
+    <li><a href="#" class="sort__button sort__button--active data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
 );
 
