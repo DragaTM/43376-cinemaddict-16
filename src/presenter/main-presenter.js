f218d1dd7b16998d65d6cb13d74fd77a1f772fe7
dragatm <dragatm@gmail.com>
2022-02-02 16:14:47 +0200

    `

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -223,7 +223,6 @@ export default class MainPresenter {
     this.#showMoreBtnComponent.setClickHandler(this.#handleShowMoreBtnClick);
   }
 
-
   #handleShowMoreBtnClick = () => {
     const filmCount = this.films.length;
     const newRenderedFilmCount = Math.min(filmCount, this.#renderedFilmCount + FILM_COUNT_PER_STEP);
